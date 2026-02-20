81593477905ff0ae692835915106153c9e78ee14
Fremont <fremontben@gmail.com>
2026-02-20 14:13:29 +0100

    Test shemas input

--- api/schemas.py
+++ api/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, Field
 from typing import Dict
 
 class PredictionInput(BaseModel):
+    AMT_CREDIT: float = Field(..., gt=0)
+    DAYS_BIRTH: int = Field(...)
     model_config = {"extra": "allow"}
 
 class PredictionOutput(BaseModel):
